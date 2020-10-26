63af9b14406454c24ea52065fd7eb470ed64fab0
Shyam Maniyedath <maniyeda@amazon.com>
2020-10-26 06:34:12 -0400

    Version 0.0.3 of the plugin

---
+++ lambda-deployment-orca/src/main/java/com/amazon/aws/spinnaker/plugin/lambda/utils/LambdaCloudDriverUtils.java
@@ -0,0 +1,352 @@
+/*
+ * Copyright 2018 Amazon.com, Inc. or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.amazon.aws.spinnaker.plugin.lambda.utils;
+
+import com.amazon.aws.spinnaker.plugin.lambda.traffic.model.LambdaCloudDriverInvokeOperationResults;
+import com.amazon.aws.spinnaker.plugin.lambda.upsert.model.LambdaDeploymentInput;
+import com.amazon.aws.spinnaker.plugin.lambda.verify.model.LambdaCloudDriverErrorObject;
+import com.amazon.aws.spinnaker.plugin.lambda.verify.model.LambdaCloudDriverTaskResults;
+import com.amazon.aws.spinnaker.plugin.lambda.verify.model.LambdaVerificationStatusOutput;
+import com.amazon.aws.spinnaker.plugin.lambda.verify.model.LambdaCloudDriverResultObject;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.IntNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.netflix.spinnaker.orca.api.pipeline.models.StageExecution;
+import com.netflix.spinnaker.orca.clouddriver.config.CloudDriverConfigurationProperties;
+import okhttp3.*;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.pf4j.util.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Component
+public class LambdaCloudDriverUtils {
+    private static final Logger logger = LoggerFactory.getLogger(LambdaCloudDriverUtils.class);
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final String CLOUDDRIVER_GET_PATH = "/functions";
+    static {
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+    }
+
+    @Autowired
+    CloudDriverConfigurationProperties props;
+
+    public LambdaCloudDriverResponse postToCloudDriver(String endPointUrl, String jsonString) {
+        RequestBody body = RequestBody.create(MediaType.parse("application/json"), jsonString);
+        Request request = new Request.Builder()
+                .url(endPointUrl)
+                .post(body)
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        Call call = client.newCall(request);
+        try {
+            Response response = call.execute();
+            String respString = response.body().string();
+            if (200 != response.code()) {
+                logger.error("Error calling cloud driver");
+                logger.error(respString);
+                throw new RuntimeException("Error calling cloud driver: " + respString);
+            }
+            logger.debug(respString);
+            LambdaCloudDriverResponse respObj = objectMapper.readValue(respString, LambdaCloudDriverResponse.class);
+            return respObj;
+        } catch (Exception e) {
+            logger.error("Error calling clouddriver.", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public LambdaCloudDriverInvokeOperationResults getLambdaInvokeResults(String endPoint) {
+        String respString = getFromCloudDriver(endPoint);
+        LambdaCloudDriverInvokeOperationResults respObject = null;
+        try {
+            JsonNode jsonResults = objectMapper.readTree(respString);
+            JsonNode statusNode = jsonResults.get("status");
+            ArrayNode resultsNode = (ArrayNode)jsonResults.get("resultObjects");
+            if ((resultsNode != null) && resultsNode.isArray()) {
+                respObject =objectMapper.convertValue(resultsNode.get(0), LambdaCloudDriverInvokeOperationResults.class);
+                JsonNode respStringNode = objectMapper.readTree(respObject.getResponseString());
+                int statusCode = ((IntNode)respStringNode.get("statusCode")).intValue();
+                String body = ((TextNode)respStringNode.get("body")).textValue();
+                respObject.setStatusCode(statusCode);
+                respObject.setBody(body);
+            }
+            LambdaVerificationStatusOutput st = objectMapper.convertValue(statusNode, LambdaVerificationStatusOutput.class);
+
+            return respObject;
+        }
+        catch (Exception e) {
+            logger.error(String.format("Failed verifying task at {}", endPoint), e);
+            return respObject;
+        }
+    }
+
+    public LambdaCloudDriverTaskResults verifyStatus(String endPoint) {
+        String respString = getFromCloudDriver(endPoint);
+        try {
+            JsonNode jsonResults = objectMapper.readTree(respString);
+            JsonNode statusNode = jsonResults.get("status");
+            ArrayNode resultsNode = (ArrayNode)jsonResults.get("resultObjects");
+            LambdaCloudDriverResultObject ro = null;
+            LambdaCloudDriverErrorObject err = null;
+            LambdaCloudDriverInvokeOperationResults respObject;
+            if ((resultsNode != null) && resultsNode.isArray()) {
+                ro = objectMapper.convertValue(resultsNode.get(0), LambdaCloudDriverResultObject.class);
+                err = objectMapper.convertValue(resultsNode.get(0), LambdaCloudDriverErrorObject.class);
+            }
+            LambdaVerificationStatusOutput st = objectMapper.convertValue(statusNode, LambdaVerificationStatusOutput.class);
+
+            return LambdaCloudDriverTaskResults.builder().results(ro).status(st).errors(err).build();
+        }
+        catch (Exception e) {
+            logger.error(String.format("Failed verifying task at {}", endPoint), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getFromCloudDriver(String endPoint) {
+        Request request = new Request.Builder()
+                .url(endPoint)
+                .get()
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        Call call = client.newCall(request);
+        try {
+            Response response = call.execute();
+            String respString = response.body().string();
+            return respString;
+        }
+        catch (Exception e) {
+            logger.error("Exception verifying task", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean lambdaExists(LambdaGetInput inp) {
+        LambdaDefinition thisLambda = retrieveLambda(inp);
+        return thisLambda != null;
+    }
+
+    public LambdaDefinition retrieveLambda(LambdaGetInput inp) {
+        //{{clouddriver_url}}/functions?functionName=a1-json_simple_lambda_222&region=us-west-2&account=aws-managed-1
+        logger.debug("Retrieve Lambda");
+        String cloudDriverUrl =  props.getCloudDriverBaseUrl();
+        String region = inp.getRegion();
+        String acc = inp.getAccount();
+        String fName = inp.getFunctionName();
+        String appPrefix = String.format("%s-", inp.getAppName());
+        if (!fName.startsWith(appPrefix)) {
+            fName = String.format("%s-%s", inp.getAppName(),inp.getFunctionName());
+        }
+        String url = cloudDriverUrl + CLOUDDRIVER_GET_PATH ;
+        HttpUrl.Builder httpBuilder = HttpUrl.parse(url).newBuilder();
+        httpBuilder.addQueryParameter("region", region);
+        httpBuilder.addQueryParameter("account", acc);
+        httpBuilder.addQueryParameter("functionName", fName);
+        Request request = new Request.Builder().url(httpBuilder.build()).build();
+        OkHttpClient client = new OkHttpClient();
+        Call call = client.newCall(request);
+        try {
+            Response response = call.execute();
+            if (200 != response.code()) {
+                logger.error("Could not retrieve lambda");
+                return null;
+            }
+            logger.debug("Found a function");
+            String respString = response.body().string();
+            LambdaDefinition lambdaDef = this.asObjectFromList(respString, LambdaDefinition.class);
+            return lambdaDef;
+        }
+        catch (Exception e) {
+            logger.error("Error calling clouddriver to find lambda.", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T getInput(StageExecution stage, Class<T> type) {
+        try {
+            T ldi = objectMapper.convertValue(stage.getContext(), type);
+            return ldi;
+        }
+        catch (Throwable e) {
+            e.printStackTrace();
+            logger.error("Could not convert value");
+        }
+        return null;
+    }
+
+    public <T> T asObjectFromList(String inpString, Class<T> type) {
+        try {
+            TypeFactory typeFactory = objectMapper.getTypeFactory();
+            List<T> someClassList = objectMapper.readValue(inpString, typeFactory.constructCollectionType(List.class, type));
+            if (someClassList.size() == 0) {
+                return null;
+            }
+            return someClassList.get(0);
+        }
+        catch (Throwable e) {
+            e.printStackTrace();
+            logger.error("Could not convert value");
+        }
+        return null;
+    }
+
+    public <T> T asObject(String inpString, Class<T> type) {
+        try {
+            TypeFactory typeFactory = objectMapper.getTypeFactory();
+            List<T> someClassList = objectMapper.readValue(inpString, typeFactory.constructCollectionType(List.class, type));
+            T ldi = objectMapper.convertValue(inpString, type);
+            return ldi;
+        }
+        catch (Throwable e) {
+            e.printStackTrace();
+            logger.error("Could not convert value");
+        }
+        return null;
+    }
+
+    public String asString(Object inp) {
+        try {
+            return objectMapper.writeValueAsString(inp);
+        } catch (JsonProcessingException e) {
+            logger.error("Could not jsonify", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getCanonicalVersion(LambdaDefinition lf, String inputVersion, String versionNumber, int retentionNumber) {
+        List<String> revisions = getSortedRevisions(lf);
+        if (revisions.size () != 0) {
+            if (inputVersion.startsWith("$PROVIDED")) {  // actual version
+                return versionNumber;
+            }
+
+            if (inputVersion.startsWith("$LATEST")) { // latest version number
+                return revisions.get(0);
+            }
+
+            if (inputVersion.startsWith("$OLDEST")) { // oldest version number
+                return revisions.get(revisions.size() - 1);
+            }
+
+            if (inputVersion.startsWith("$PREVIOUS")) { // latest - 1 version number
+                if (revisions.size() >= 2)
+                    return revisions.get(1);
+                else
+                    return null;
+            }
+
+            if (inputVersion.startsWith("$MOVING")) { // list of versions
+                if (revisions.size() > retentionNumber) {
+                    List<String> toRemoveList = revisions.subList(retentionNumber, revisions.size());
+                    return String.join(",", toRemoveList);
+                }
+            }
+            // Couldnt find it.
+            logger.error(String.format("Found invalid version string %s", inputVersion));
+            return null;
+        }
+        logger.error("No published versions exist for function.");
+        return null;
+    }
+
+    public List<String> getSortedRevisions(LambdaDefinition lf) {
+        List<String> revisions =  lf.getRevisions().values().stream().collect(Collectors.toList());
+        List<Integer> revInt = revisions.stream()
+                                        .filter(x -> { return NumberUtils.isCreatable(x); })
+                                        .map(x -> { return Integer.valueOf(x); })
+                                        .collect(Collectors.toList());
+        revInt = revInt.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        List<String> answers = revInt.stream().map( x -> {
+            return Integer.toString(x);}).collect(Collectors.toList());
+        return answers;
+    }
+
+    public LambdaDefinition findLambda(StageExecution stage) {
+        return findLambda(stage, false);
+    }
+
+    public LambdaDefinition findLambda(StageExecution stage, boolean shouldRetry) {
+        LambdaGetInput lgi = this.getInput(stage, LambdaGetInput.class);
+        lgi.setAppName(stage.getExecution().getApplication());
+        //LambdaGetOutput lf = (LambdaGetOutput)stage.getContext().get(LambdaStageConstants.lambdaObjectKey);
+        LambdaDefinition lf = this.retrieveLambda(lgi);
+        int count = 0;
+        while (lf == null && count < 5 && shouldRetry == true) {
+            count++;
+            lf = this.retrieveLambda((lgi));
+            this.await();
+        }
+        return lf;
+    }
+
+    public boolean validateUpsertLambdaInput(LambdaDeploymentInput inputLambda, List<String> errorMessages) {
+        if (!ObjectUtils.defaultIfNull(inputLambda.getEnableLambdaAtEdge(), Boolean.FALSE)) {
+            return true;
+        }
+        return validateLambdaEdgeInput(inputLambda, errorMessages);
+    }
+
+    public boolean validateLambdaEdgeInput(LambdaDeploymentInput inputLambda, List<String> errorMessages) {
+        int numErrors = errorMessages.size();
+
+        if (inputLambda.getEnvVariables() == null || inputLambda.getEnvVariables().size() > 0) {
+            errorMessages.add("Edge enabled lambdas cannot have env variables");
+        }
+        if (inputLambda.getTimeout() > 5) {
+            errorMessages.add("Edge enabled lambdas cannot have timeout > 5");
+        }
+        if (inputLambda.getMemorySize() > 128) {
+            errorMessages.add("Edge enabled lambdas cannot have memory > 128");
+        }
+        if (!inputLambda.getRegion().equals("us-east-1")) {
+            errorMessages.add("Edge enabled lambdas need to be deployed in us-east-1 region");
+        }
+        if (StringUtils.isNotNullOrEmpty(inputLambda.getVpcId())) {
+            errorMessages.add("Edge enabled lambdas cannot have vpc associations");
+        }
+        if (inputLambda.getSubnetIds() == null || inputLambda.getSubnetIds().size() > 0) {
+            errorMessages.add("Edge enabled lambdas cannot have subnets");
+        }
+        if (inputLambda.getSecurityGroupIds() == null || inputLambda.getSecurityGroupIds().size() > 0) {
+            errorMessages.add("Edge enabled lambdas cannot have security groups");
+        }
+        return errorMessages.size() == numErrors;
+    }
+
+    public void await() {
+        try {
+            logger.debug("Going to sleep during lambda");
+            Thread.sleep(20000);
+        }
+        catch (Throwable e) {
+            logger.error("Error during await of lambda ", e);
+        }
+    }
+}
---
+++ lambda-deployment-orca/src/main/java/com/amazon/aws/spinnaker/plugin/lambda/verify/LambdaCacheRefreshTask.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2018 Amazon.com, Inc. or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.amazon.aws.spinnaker.plugin.lambda.verify;
+
+import com.amazon.aws.spinnaker.plugin.lambda.LambdaStageBaseTask;
+import com.amazon.aws.spinnaker.plugin.lambda.upsert.LambdaPutConcurrencyTask;
+import com.amazon.aws.spinnaker.plugin.lambda.utils.LambdaCloudDriverUtils;
+import com.netflix.spinnaker.orca.api.pipeline.Task;
+import com.netflix.spinnaker.orca.api.pipeline.TaskResult;
+import com.netflix.spinnaker.orca.api.pipeline.models.ExecutionStatus;
+import com.netflix.spinnaker.orca.api.pipeline.models.StageExecution;
+import com.netflix.spinnaker.orca.clouddriver.CloudDriverCacheService;
+import com.netflix.spinnaker.orca.clouddriver.tasks.AbstractCloudProviderAwareTask;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import retrofit.client.Response;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class LambdaCacheRefreshTask implements LambdaStageBaseTask {
+    private static Logger logger = LoggerFactory.getLogger(LambdaCacheRefreshTask.class);
+    static final String REFRESH_TYPE = "Function";
+
+    @Autowired
+    private CloudDriverCacheService cacheService;
+
+    @Autowired
+    private LambdaCloudDriverUtils utils;
+
+    @Nonnull
+    @Override
+    public TaskResult execute(@Nonnull StageExecution stage) {
+        logger.debug("Executing LambdaCacheRefreshTask...");
+        prepareTask(stage);
+        Map<String, Object> task = new HashMap<>(stage.getContext());
+        task.put("appName", stage.getExecution().getApplication());
+        cacheService.forceCacheUpdate("aws", REFRESH_TYPE, task);
+        logger.debug("Going to wait for some seconds after requesting cache refresh...");
+        utils.await();
+        return TaskResult.ofStatus(ExecutionStatus.SUCCEEDED);
+    }
+}
