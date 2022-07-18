78c76300597dbf36fee230783f75069c5de76cb0
Avi1235 <david-prz98@hotmail.com>
2022-07-18 11:57:32 -0600

    Added unit test for LambdaTrafficUpdateVerificationTask

---
+++ lambda-deployment-orca/src/test/java/com/amazon/aws/spinnaker/plugin/lambda/traffic/LambdaTrafficUpdateVerificationTaskTest.java
@@ -0,0 +1,124 @@
+package com.amazon.aws.spinnaker.plugin.lambda.traffic;
+
+import com.amazon.aws.spinnaker.plugin.lambda.traffic.model.LambdaTrafficUpdateInput;
+import com.amazon.aws.spinnaker.plugin.lambda.utils.LambdaCloudDriverResponse;
+import com.amazon.aws.spinnaker.plugin.lambda.utils.LambdaCloudDriverUtils;
+import com.amazon.aws.spinnaker.plugin.lambda.utils.LambdaDefinition;
+import com.amazon.aws.spinnaker.plugin.lambda.verify.model.LambdaCloudDriverErrorObject;
+import com.amazon.aws.spinnaker.plugin.lambda.verify.model.LambdaCloudDriverTaskResults;
+import com.amazon.aws.spinnaker.plugin.lambda.verify.model.LambdaVerificationStatusOutput;
+import com.amazonaws.services.lambda.model.AliasConfiguration;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.netflix.spinnaker.orca.api.pipeline.models.ExecutionStatus;
+import com.netflix.spinnaker.orca.api.pipeline.models.PipelineExecution;
+import com.netflix.spinnaker.orca.api.pipeline.models.StageExecution;
+import com.netflix.spinnaker.orca.clouddriver.config.CloudDriverConfigurationProperties;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import ru.lanwen.wiremock.ext.WiremockResolver;
+import ru.lanwen.wiremock.ext.WiremockUriResolver;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith({WiremockResolver.class, WiremockUriResolver.class})
+public class LambdaTrafficUpdateVerificationTaskTest {
+
+    WireMockServer wireMockServer;
+
+    @InjectMocks
+    private LambdaTrafficUpdateVerificationTask lambdaTrafficUpdateVerificationTask;
+
+    @Mock
+    private CloudDriverConfigurationProperties propsMock;
+
+    @Mock
+    private LambdaCloudDriverUtils lambdaCloudDriverUtilsMock;
+
+    @Mock
+    private StageExecution stageExecution;
+
+    @Mock
+    private PipelineExecution pipelineExecution;
+
+    @BeforeEach
+    void init(@WiremockResolver.Wiremock WireMockServer wireMockServer, @WiremockUriResolver.WiremockUri String uri) {
+        this.wireMockServer = wireMockServer;
+        MockitoAnnotations.initMocks(this);
+        Mockito.when(propsMock.getCloudDriverBaseUrl()).thenReturn(uri);
+        pipelineExecution.setApplication("lambdaApp");
+        Mockito.when(stageExecution.getExecution()).thenReturn(pipelineExecution);
+        Mockito.when(stageExecution.getContext()).thenReturn(new HashMap<String, Object>());
+        Mockito.when(stageExecution.getOutputs()).thenReturn(new HashMap<String, Object>());
+        stageExecution.getContext().put("url", "test");
+        LambdaTrafficUpdateInput ldi = LambdaTrafficUpdateInput.builder()
+                .functionName("functionName")
+                .aliasName("develop")
+                .deploymentStrategy("$BLUEGREEN")
+                .build();
+        Mockito.when(lambdaCloudDriverUtilsMock.postToCloudDriver(Mockito.any(),Mockito.any() )).thenReturn(LambdaCloudDriverResponse.builder().build());
+        Mockito.when(lambdaCloudDriverUtilsMock.getInput(stageExecution, LambdaTrafficUpdateInput.class)).thenReturn(ldi);
+    }
+
+    @Test
+    public void execute_UpdateVerification_verifyStatus_RUNNING(){
+        LambdaCloudDriverTaskResults lambdaCloudDriverTaskResults = LambdaCloudDriverTaskResults.builder()
+                .status(LambdaVerificationStatusOutput.builder().status("RUNNING").build())
+                .build();
+        Mockito.when(lambdaCloudDriverUtilsMock
+                .verifyStatus(Mockito.any())).thenReturn(lambdaCloudDriverTaskResults);
+
+        assertEquals(ExecutionStatus.RUNNING, lambdaTrafficUpdateVerificationTask.execute(stageExecution).getStatus());
+    }
+
+    @Test
+    public void execute_UpdateVerification_verifyStatus_ERROR(){
+        LambdaCloudDriverTaskResults lambdaCloudDriverTaskResults = LambdaCloudDriverTaskResults.builder()
+                .status(LambdaVerificationStatusOutput.builder()
+                        .completed(true)
+                        .failed(true)
+                        .status("TERMINAL")
+                        .build())
+                .errors(LambdaCloudDriverErrorObject.builder()
+                        .message("clouddriver task not found error")
+                        .build())
+                .build();
+        Mockito.when(lambdaCloudDriverUtilsMock
+                .verifyStatus(Mockito.any())).thenReturn(lambdaCloudDriverTaskResults);
+
+        assertEquals(ExecutionStatus.TERMINAL, lambdaTrafficUpdateVerificationTask.execute(stageExecution).getStatus());
+        assertEquals("clouddriver task not found error", stageExecution.getOutputs().get("failureMessage"));
+    }
+
+    @Test
+    public void execute_UpdateVerification_validateWeights(){
+        List<AliasConfiguration> aliasConfigurationList = new ArrayList<>();
+        //RoutingConfig() will be null ending while loop
+        aliasConfigurationList.add(new AliasConfiguration().withName("develop"));
+
+        LambdaCloudDriverTaskResults lambdaCloudDriverTaskResults = LambdaCloudDriverTaskResults.builder()
+                .status(LambdaVerificationStatusOutput.builder()
+                        .status("RUNNING")
+                        .completed(true)
+                        .build())
+                .build();
+        Mockito.when(lambdaCloudDriverUtilsMock
+                .verifyStatus(Mockito.any())).thenReturn(lambdaCloudDriverTaskResults);
+        LambdaDefinition lambdaDefinition = LambdaDefinition.builder()
+                .aliasConfigurations(aliasConfigurationList)
+                .build();
+        Mockito.when(lambdaCloudDriverUtilsMock
+                .retrieveLambdaFromCache(stageExecution, false)).thenReturn(lambdaDefinition);
+
+        assertEquals(ExecutionStatus.SUCCEEDED, lambdaTrafficUpdateVerificationTask.execute(stageExecution).getStatus());
+    }
+
+}
